46827a0dcd1fb0a195fb2404417ce03c4862dcd3
vucuong12 <vucuong020195@gmail.com>
2017-04-23 17:06:24 +0900

    add navigation bar

---
+++ js/config.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDgTgfZaVe91_ocsgELO4CrH9o7E23x9C0",
+  authDomain: "fastgift-679c3.firebaseapp.com",
+  databaseURL: "https://fastgift-679c3.firebaseio.com",
+  projectId: "fastgift-679c3",
+  storageBucket: "fastgift-679c3.appspot.com",
+  messagingSenderId: "585211928797"
+};
+firebase.initializeApp(config);
+console.log(firebase);
+
