f0f75c87678997e0f23fa3ecbb92356912699632
vucuong12 <vucuong020195@gmail.com>
2017-05-08 21:30:32 +0900

    fix js for resizing

--- js/bigbang.js
+++ js/bigbang.js
@@ -1,11 +1,13 @@
 $(document).ready(function()
 {
+	showSlide(1);
 	/*SLIDE 2*/
 
 	var curImgHeight = $("#balloon-img").height();
 	var curImgWidth = $("#balloon-img").width();
 
 	function adjustInput567() {
+		console.log("adjustInput567");
 		var topImg = $("#balloon-img").offset().top;
 		var leftImg = $("#balloon-img").offset().left;
 		var newImgHeight = $("#balloon-img").height();
@@ -13,53 +15,50 @@ $(document).ready(function()
 		var ratio = newImgWidth / curImgWidth;
 		//Input 5
 		var input5 = $("#wrapper_input5");
-		input5.height(input5.height() * ratio);
-		input5.width(input5.width() * ratio);
+		input5.height(newImgHeight * 0.3);
+		input5.width(newImgWidth * 0.3);
 		input5.offset({top: topImg + 0.2 * newImgHeight, left: leftImg + 0.01 * newImgWidth})
 
 		//Input 6
 		var input6 = $("#wrapper_input6");
-		input6.height(input6.height() * ratio);
-		input6.width(input6.width() * ratio);
+		input6.height(newImgHeight * 0.3);
+		input6.width(newImgWidth * 0.3);
 		input6.offset({top: topImg + 0.05 * newImgHeight, left: leftImg + 0.39 * newImgWidth})
 
 		//Input 7
 		var input7 = $("#wrapper_input7");
-		input7.height(input7.height() * ratio);
-		input7.width(input7.width() * ratio);
+		input7.height(newImgHeight * 0.3);
+		input7.width(newImgWidth * 0.3);
 		input7.offset({top: topImg + 0.2 * newImgHeight, left: leftImg + 0.8 * newImgWidth})
 
 		curImgHeight = $("#balloon-img").height();
 		curImgWidth = $("#balloon-img").width();
 	}
 
-	if ($('#panel2').css('display') != 'none') {
-		adjustInput567();
-		$( window ).resize(function() {
-		  adjustInput567();
-		});    
-	}
-
-
-
-
+	afterResize(function(){
+		console.log("Resize on panal 2")
+		if ($('#panel2').css('display') != 'none'){
+			adjustInput567();	
+		}
+		
+	})
 
 	/*SLIDE 3*/
 	var curCardHeight = $("#gift_card_panel3").height();
 	var curCardWidth = $("#gift_card_panel3").width();
 	
 	function adjustInput9() {
+
 		var topImg = $("#gift_card_panel3").offset().top;
 		var leftImg = $("#gift_card_panel3").offset().left;
 		var newCardHeight = $("#gift_card_panel3").height();
 		var newCardWidth = $("#gift_card_panel3").width();
-		var ratio = newCardWidth / curCardWidth;
-		
+
 		//Input 9
 		var input9 = $("#wrapper_input9");
-		input9.height(input9.height() * ratio );
-		input9.width(input9.width() * ratio );
-		input9.offset({top: topImg + 0.5 * newCardHeight, left: leftImg + 0.3 * newCardWidth})
+		input9.height(newCardHeight * 0.3 );
+		input9.width(newCardHeight * 0.6 );
+		input9.offset({top: topImg + 0.47 * newCardHeight, left: leftImg + 0.35 * newCardHeight})
 
 		
 
@@ -67,13 +66,25 @@ $(document).ready(function()
 		curCardWidth = $("#gift_card_panel3").width();
 	}
 
-	
-	if ($('#panel3').css('display') != 'none') {
+	afterResize(function(){
+		console.log("Resize on panal 3")
+		if ($('#panel3').css('display') != 'none'){
+			adjustInput9();	
+		}
 		
-		adjustInput9();
-		$( window ).resize(function() {
-		  adjustInput9();
-		});    
+	})
+
+	function afterResize(callback){
+		var delta = 10;
+		var id;
+		$(window).resize(function() {
+			clearTimeout(id);
+		    id = setTimeout(resizeend, delta);
+		});
+
+		function resizeend() {
+		    callback();              
+		}
 	}
 
 	$("#slide1").click(function(){
@@ -91,6 +102,9 @@ $(document).ready(function()
 	
 
 	function showSlide(slideNumber, totalSlide = 3){
+		if ($('#panel' + slideNumber).css('display') != 'none') {
+			return;
+		}
 		for (var i = 1; i < totalSlide + 1; i++){
 			if (i == slideNumber){
 				$("#panel" + i).show();	
@@ -101,8 +115,14 @@ $(document).ready(function()
 			}
 			
 		}
+
+		if (slideNumber == 3){
+			adjustInput9();	
+		} else if (slideNumber == 2){
+			adjustInput567();
+		}
 	}
-	showSlide(1);
+	
 	$("#panel1_next").click(function()
 	{
 		showSlide(2);
